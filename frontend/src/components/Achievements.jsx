dfc34447876ddf34e9014c717b52f8a537889238
Nikita Menon <53401077+nikxtaco@users.noreply.github.com>
2020-03-12 11:25:17 +0530

    Revert "updates"

--- frontend/src/components/Achievements.jsx
+++ frontend/src/components/Achievements.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
-
 const Achievements = () => {
 	return (
-		<section className="achievements section" id="section_spacing">
+		<section className="achievements section">
 			<div className="container">
-				<h3 className='grey-text text-darken-3 center-align' id="bottom_margin">Achievements</h3>
+				<h3 className='grey-text text-darken-3 center-align'>Achievements</h3>
 				<div className="row ">
-				
-					<div className="col s12 m3">
-						<div style={{ 'min-height': '41.5vh' }} className="card" id="curve_border">
+					<div className="col s6 m3">
+						<div className="card">
 							<div className="card-image">
 								{/* <img src={require('../Assets/sample.jpeg')} alt='1'> */}
-								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" id="curve_border_top"/>
+								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" />
 								<span className="card-title">Card Title</span>
 							</div>
 							<div className="card-content">
@@ -24,11 +22,11 @@ const Achievements = () => {
 						</div>
 					</div>
 
-					<div className="col s12 m3">
-						<div style={{ 'min-height': '41.5vh' }} className="card" id="curve_border">
+					<div className="col s6 m3">
+						<div className="card">
 							<div className="card-image">
 								{/* <img src={require('../Assets/sample.jpeg')} alt='1'> */}
-								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" id="curve_border_top"/>
+								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" />
 								<span className="card-title">Card Title</span>
 							</div>
 							<div className="card-content">
@@ -40,11 +38,11 @@ const Achievements = () => {
 						</div>
 					</div>
 
-					<div className="col s8 m3">
-						<div style={{ 'min-height': '41.5vh' }} className="card" id="curve_border">
+					<div className="col s6 m3">
+						<div className="card">
 							<div className="card-image">
 								{/* <img src={require('../Assets/sample.jpeg')} alt='1'> */}
-								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" id="curve_border_top"/>
+								<img src={require('../Assets/sample.jpeg')} alt="1" className="responsive-img" />
 								<span className="card-title">Card Title</span>
 							</div>
 							<div className="card-content">
@@ -56,8 +54,8 @@ const Achievements = () => {
 						</div>
 					</div>
 
-					<div className="col s4 m3 valign-wrapper" id="curve_border">
-						<div style={{ 'min-height': '41.5vh', 'width':'100%', 'text-align':'center' }} className="card-panel  grey waves-effect" id="curve_border">
+					<div className="col s6 m3 valign-wrapper ">
+						<div style={{ 'min-height': '41.5vh' }} className="card-panel  grey waves-effect">
 								<i className="material-icons right">arrow_forward</i>
 								See More
 						</div>
--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import M from 'materialize-css/dist/js/materialize.min.js';
-import "../index.css";
 
 const Gallery = () => {
 
@@ -12,39 +11,49 @@ const Gallery = () => {
 	return (
 		<div className='gallery section'>
 			<div className="container center">
-				<h3 className='grey-text text-darken-3 center-align' id="bottom_margin">Gallery</h3>
+				<h3 className='grey-text text-darken-3 center-align'>Gallery</h3>
 				<div class="row">
 					<div class="col s12 m3">
-						<img style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" className="materialboxed responsive-img mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 					<div class="col s12 m3">
-						<img style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 					<div class="col s12 m3">
-						<img style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 					<div class="col s12 m3">
-						<img style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 				</div>
 				<div class="row">
 					<div class="col s12 m3">
-						<img style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 					<div class="col s12 m3">
-						<img  style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 					<div class="col s12 m3">
-						<img  style={{ 'min-height': '25vh', 'width':'100%' }} src={require("../Assets/s2.jpeg")} alt="" class="materialboxed mobile_space"  id="curve_border"/>
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
-					<div className="col s12 m3" id="curve_border">
-						<div style={{ 'min-height': '25vh', 'width':'100%', 'margin-top':'0' }} className="card-panel  grey waves-effect" id="curve_border">
-								<i className="material-icons right">arrow_forward</i>
-								See More
-						</div>
+					<div class="col s12 m3">
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
 					</div>
 				</div>
-				
+				<div class="row">
+					<div class="col s12 m3">
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
+					</div>
+					<div class="col s12 m3">
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
+					</div>
+					<div class="col s12 m3">
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
+					</div>
+					<div class="col s12 m3">
+						<img src={require("../Assets/s2.jpeg")} alt="" class="materialboxed responsive-img" />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
