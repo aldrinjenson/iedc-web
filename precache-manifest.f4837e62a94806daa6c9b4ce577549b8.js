a837464b6a72c0148f43b37757502f3983888770
Vivek R <123vivekr@gmail.com>
2020-03-21 15:38:05 +0530

    Updates

---
+++ precache-manifest.f4837e62a94806daa6c9b4ce577549b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54712e1fb47cd4409d202419ffa6857",
+    "url": "/iedc-web/index.html"
+  },
+  {
+    "revision": "996d61533d3fad6bdf2e",
+    "url": "/iedc-web/static/css/2.2920fd17.chunk.css"
+  },
+  {
+    "revision": "2d12771798bc7721a6bd",
+    "url": "/iedc-web/static/css/main.0f56b920.chunk.css"
+  },
+  {
+    "revision": "996d61533d3fad6bdf2e",
+    "url": "/iedc-web/static/js/2.1ac11f3d.chunk.js"
+  },
+  {
+    "revision": "88d6579bf2c5a437a0c5be414f667d3d",
+    "url": "/iedc-web/static/js/2.1ac11f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d12771798bc7721a6bd",
+    "url": "/iedc-web/static/js/main.a20e1214.chunk.js"
+  },
+  {
+    "revision": "7f95c02dc5614ac4e7da",
+    "url": "/iedc-web/static/js/runtime-main.e295be6b.js"
+  },
+  {
+    "revision": "bd21a28cb5c9ea4f34e6874d3b9e51cf",
+    "url": "/iedc-web/static/media/about.bd21a28c.png"
+  },
+  {
+    "revision": "463898dd4f4b1ddada780c05c8ff2ada",
+    "url": "/iedc-web/static/media/ache1.463898dd.png"
+  },
+  {
+    "revision": "b7d4d11dd13bbed992f667aa6c09c039",
+    "url": "/iedc-web/static/media/ache2.b7d4d11d.png"
+  },
+  {
+    "revision": "a373f05ce3a8701fbce3f1dd6602d134",
+    "url": "/iedc-web/static/media/ache3.a373f05c.png"
+  },
+  {
+    "revision": "dc32e46fd4f845ce5ed9386a60ab199d",
+    "url": "/iedc-web/static/media/gallery1.dc32e46f.png"
+  },
+  {
+    "revision": "8f5303c724e472fe7e277e2e61782471",
+    "url": "/iedc-web/static/media/head.8f5303c7.png"
+  },
+  {
+    "revision": "a3c6d5b0c7604da87eac0bea297e6fc4",
+    "url": "/iedc-web/static/media/logo.a3c6d5b0.svg"
+  },
+  {
+    "revision": "b8768ca073fa24d6d49b387700cd2706",
+    "url": "/iedc-web/static/media/t.b8768ca0.jfif"
+  }
+]);
